5dc1af4b79284e43dc1a3845a5b2cfed0f335414
Roberto Cesar <rcesar.vitorio@gmail.com>
2025-07-18 01:31:03 -0300

    Refactor webhook handler to return payment data and success message in response

--- api/webhook.js
+++ api/webhook.js
@@ -26,7 +26,10 @@ export default async function handler(req, res) {
 
         // Aqui você pode salvar no banco, enviar e-mail etc.
 
-        return res.status(200).end();
+        return res.status(200).json({
+          data: paymentData,
+          message: 'Webhook processado com sucesso.',
+        });
       }
 
       return res.status(204).end(); // outro tipo de notificação
